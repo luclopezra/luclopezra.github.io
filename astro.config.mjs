8b1ee717e9a5b970ba0840d6b44daf80044063c3
Nik Klemenc <38212799+Scorpio3310@users.noreply.github.com>
2025-09-04 18:09:30 +0200

    Add Svelte integration and Counter component

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,22 +7,21 @@ import tailwindcss from "@tailwindcss/vite";
 
 import sitemap from "@astrojs/sitemap";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
     // UPDATE THIS FOR PRODUCTION – This will also be used in the sitemap
     site: "http://localhost:4321",
-    integrations: [
-        mdx(),
-        sitemap({
-            customPages: [
-                "http://localhost:4321", // home page - priority 1.0
-                "http://localhost:4321/sl/", // Slovenian home - priority 1.0
-            ],
-            changefreq: "monthly",
-            priority: 0.7,
-            lastmod: new Date(),
-        }),
-    ],
+    integrations: [mdx(), sitemap({
+        customPages: [
+            "http://localhost:4321", // home page - priority 1.0
+            "http://localhost:4321/sl/", // Slovenian home - priority 1.0
+        ],
+        changefreq: "monthly",
+        priority: 0.7,
+        lastmod: new Date(),
+    }), svelte()],
 
     vite: {
         plugins: [tailwindcss()],
